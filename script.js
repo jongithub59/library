43f2d25aade39abdc7d3a0800ef0fbef5dcf428b
jongithub59 <jdelrealj01@gmail.com>
2023-12-11 11:23:53 -0600

    added filler text for .book for addBookToLibrary() added functionality to read button and set up addBookToLibrary() for loop to add content to book elements

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const library = []
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read')
+const harryPotter = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.k Rowling', 223, 'not read')
+const illiad = new Book('Illiad', 'Homer', '+'+700, 'not read') 
+const dune = new Book('Dune', 'Frank Herbert', 896, 'read')
+const odyssey = new Book('Odyssey', 'Homer', '+'+380, 'not read')
+const hamlet = new Book('Hamlet', 'William Shakespeare', 330, 'read')
+
+library[0] = theHobbit
+library[1] = illiad
+library[2] = odyssey
+library[3] = dune
+library[4] = hamlet
+library[5] = harryPotter
+
+const addBook = document.getElementById('add-button')
+const readButtons = document.querySelectorAll('.isRead')
+
+//chnages read button to either 'read' or 'not read' and its color
+readButtons.forEach( (button) => {  
+    button.addEventListener('click', () => {
+        if (button.classList.contains('read')) {
+            button.classList.remove('read')
+            button.classList.add('not-read')
+            button.textContent = 'Not Read'
+        } else { 
+            button.classList.remove('not-read')
+            button.classList.add('read')
+            button.textContent = 'Read'
+        }
+    })
+})
+
+addBook.addEventListener('click', addBookToLibrary)
+
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -13,13 +49,14 @@ function Book(title, author, pages, read) {
     //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}` 
     // }
 }
+function addBookToLibrary() {
+    for (i in library)
+    console.log(library[i].info())
+}
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read')
-const harryPotter = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.k Rowling', 223, 'not read')
-const illiad = new Book('Illiad', 'Homer', '+'+700, 'not read') 
-const dune = new Book('Dune', 'Frank Herbert', 896, 'read')
-const odyssey = new Book('Odyssey', 'Homer', '+'+380, 'not read')
-const hamlet = new Book('Hamlet', 'William Shakespeare', 330, 'read')
+
+
+
 
 
 

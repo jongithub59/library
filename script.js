ea2e8505f0fd926472c3d9145cd12e8711e8ce18
jongithub59 <jdelrealj01@gmail.com>
2023-12-12 10:44:56 -0600

    added logic for adding book content to the page added code that takes info from a Book object and adds it to elements that is then given classes for styling and appended to main. Need to rework it to work with dialog inputs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const library = []
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read')
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Not Read')
 const harryPotter = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.k Rowling', 223, 'not read')
 const illiad = new Book('Illiad', 'Homer', '+'+700, 'not read') 
 const dune = new Book('Dune', 'Frank Herbert', 896, 'read')
@@ -8,17 +8,19 @@ const odyssey = new Book('Odyssey', 'Homer', '+'+380, 'not read')
 const hamlet = new Book('Hamlet', 'William Shakespeare', 330, 'read')
 
 library[0] = theHobbit
-library[1] = illiad
-library[2] = odyssey
-library[3] = dune
-library[4] = hamlet
-library[5] = harryPotter
+// library[1] = illiad
+// library[2] = odyssey
+// library[3] = dune
+// library[4] = hamlet
+// library[5] = harryPotter
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+
+
 
     //shortened arrow funtion
     this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}` 
@@ -34,6 +36,9 @@ const readButtons = document.querySelectorAll('.isRead')
 const dialog = document.getElementById('book-dialog')
 const cancel = document.getElementById('cancel')
 const confirmBook = document.getElementById('confirm')
+const main = document.querySelector('.main')
+
+console.log(theHobbit.title)
 
 addBook.addEventListener('click', () => {
     dialog.showModal()
@@ -42,7 +47,6 @@ addBook.addEventListener('click', () => {
 confirmBook.addEventListener('click', addBookToLibrary)
 
 cancel.addEventListener('click', () => {
-
     dialog.close()
 })
 
@@ -64,8 +68,41 @@ readButtons.forEach( (button) => {
 
 
 function addBookToLibrary() {
-    for (i in library)
-    console.log(library[i].info())
+    let book = document.createElement('div')
+    book.classList.add('book')
+
+    let titleElement = document.createElement('p')
+    const title = theHobbit.title
+    titleElement.innerText = `"${title}"`
+
+    let authorElement = document.createElement('p')
+    const author = theHobbit.author
+    authorElement.innerText = author 
+
+    let pagesElement = document.createElement('p')
+    const pages = theHobbit.pages
+    pagesElement.innerText = `${pages} pages`
+
+    let readElement = document.createElement('button')
+    const read = theHobbit.read
+    if (theHobbit.read == 'Not Read') {
+    readElement.classList.add('not-read')
+    } else {
+        readElement.classList.add('read')
+    } 
+    readElement.innerText = read
+
+    let deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete'
+    deleteButton.classList.add('delete')
+    
+
+    main.appendChild(book)
+    book.appendChild(titleElement)
+    book.appendChild(authorElement)
+    book.appendChild(pagesElement)
+    book.appendChild(readElement)
+    book.appendChild(deleteButton)
 }
 
 
